8a3fab17e3ef570226d19f20ce32765919da946d
Piotr Sołtysek <p.soltysek@gmail.com>
2020-07-12 21:51:05 +0200

    commit test

--- src/main/java/pl/camp/it/model/Product.java
+++ src/main/java/pl/camp/it/model/Product.java
@@ -1,71 +1,16 @@
 package pl.camp.it.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Product {
+    @Id
     private int id;
     private String name;
     private int amount;
     private long barcode;
     private ProductCategory category;
 
-    public Product(int id, String name, int amount, long barcode, ProductCategory category) {
-        this.id = id;
-        this.name = name;
-        this.amount = amount;
-        this.barcode = barcode;
-        this.category = category;
-    }
 
-    public Product() {
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAmount() {
-        return amount;
-    }
-
-    public void setAmount(int amount) {
-        this.amount = amount;
-    }
-
-    public long getBarcode() {
-        return barcode;
-    }
-
-    public void setBarcode(long barcode) {
-        this.barcode = barcode;
-    }
-
-    public ProductCategory getCategory() {
-        return category;
-    }
-
-    public void setCategory(ProductCategory category) {
-        this.category = category;
-    }
-
-    @Override
-    public String toString() {
-        return "Product{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", amount=" + amount +
-                ", barcode=" + barcode +
-                ", category='" + category.getName() + '\'' +
-                '}';
-    }
 }
--- src/main/java/pl/camp/it/model/ProductCategory.java
+++ src/main/java/pl/camp/it/model/ProductCategory.java
@@ -1,47 +1,13 @@
 package pl.camp.it.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class ProductCategory {
+    @Id
     int id;
     String name;
     boolean deleted;
 
-    public ProductCategory(int id, String name, boolean deleted) {
-        this.id = id;
-        this.name = name;
-        this.deleted = deleted;
-    }
-
-    public ProductCategory() {
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public boolean isDeleted() {
-        return deleted;
-    }
-
-    public void setDeleted(boolean deleted) {
-        this.deleted = deleted;
-    }
-
-    @Override
-    public String toString() {
-        return "ProductCategory{" +
-                "name='" + name + '\'' +
-                '}';
-    }
 }
